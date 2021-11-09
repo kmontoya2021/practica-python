772ad6f7f24416642b6f8be7df468d7c69e683d5
unknown <karendeblanco2017@gmail.com>
2021-11-09 09:39:00 -0600

    first commit

---
+++ variables.py
@@ -0,0 +1,26 @@
+nombre ="karen"
+
+print(nombre)
+
+#case sencitive
+#diferentes de mayuscula de minuscula
+
+#definir varias variables
+
+x, book = 100, "Robot"
+print(x, book)
+
+#convencion
+
+#valores que no cambian se escriben en mayuscula
+
+PI = 3.1416
+
+
+#otra convencion 
+
+NombreCompleto  #camelcase
+nombre_Completo   #snakecase
+
+#las variables pueden ser reasignadas
+
